d8dc341f52efba256102071cf96a9e8986eb8471
Madhan R <135804250+madhanr13@users.noreply.github.com>
2025-01-19 12:08:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/video/:id" element={<Video />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+  return <div className="bg-yt-black h-14 flex items-center pl-4 pr-5 justify-between fixed w-full z-10 ">Navbar</div>;
+}
+
+export default Navbar;
