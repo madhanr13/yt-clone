9f17c67338678e39491b03a73e2e6d14028639d3
Madhan R <135804250+madhanr13@users.noreply.github.com>
2025-01-21 13:26:56 +0530

    Enhance video component with comment functionality and user authentication

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,30 +1,67 @@
-import { doc, onSnapshot, query } from "firebase/firestore";
+import { addDoc, collection, doc, onSnapshot, query } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { db } from "../firebase";
+import { auth, db, timestamp } from "../firebase";
 import { AiFillLike } from "react-icons/ai";
 import { RiShareForwardLine } from "react-icons/ri";
 import { HiDotsHorizontal, HiDownload } from "react-icons/hi";
 import { MdOutlineSort } from "react-icons/md";
 import { BiDislike } from "react-icons/bi";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser, setUser } from "../slices/userSlice";
+import { onAuthStateChanged } from "firebase/auth";
 
 const Video = () => {
   const [videos, setVideos] = useState([]);
   const [comments, setComments] = useState([]);
   const [data, setData] = useState([]);
+  const [comment, setComment] = useState("");
 
   const { id } = useParams();
+  const dispatch = useDispatch();
+  const user = useSelector(getUser);
 
   useEffect(() => {
-    document.title = `${data.name}   |   Youtube`;
+    document.title = `${data?.name}   |   Youtube`;
     if (id) {
       const q = query(doc(db, "videos", id));
       onSnapshot(q, (snapshot) => {
         setData(snapshot.data());
       });
+      const commentsQuery = query(collection(db, "videos", id, "comments"));
+      onSnapshot(commentsQuery, (snapshot) => {
+        setComments(
+          snapshot.docs.map((doc) => ({
+            ...data.doc(),
+            id: doc.id,
+          }))
+        );
+      });
     }
+  }, [id]);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser(user));
+      } else {
+        dispatch(setUser(null));
+      }
+    });
   }, []);
-  console.log(data);
+
+  const addComment = async (e) => {
+    e.preventDefault();
+    let commentData = {
+      image: data?.photoURL,
+      name: data?.displayName,
+      comment,
+      uploaded: timestamp,
+    };
+    if (id) {
+      await addDoc(collection(db, "videos", id, "comments"), commentData);
+      setComment("")
+    }
+  };
 
   return (
     <div className="py-20 px-9 bg-yt-black flex flex-row h-full">
@@ -35,7 +72,6 @@ const Video = () => {
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
             allowFullScreen
             className="w-[850px] h-[700px] flex-1"
           ></iframe>
@@ -106,7 +142,9 @@ const Video = () => {
             <p className="font-medium pr-3">{data?.uploadTime}</p>
           </div>
           <span className="text-center font-medium">
-            {data?.description && data?.description.length <=1000 ? data?.description : `${data?.description && data?.description.substring(0,600)}`}
+            {data?.description && data?.description.length <= 1000
+              ? data?.description
+              : `${data?.description && data?.description.substring(0, 600)}`}
           </span>
         </div>
         <div className="text-yt-white mt-5 ">
@@ -117,6 +155,26 @@ const Video = () => {
               <p className="text-sm font-medium text-yt-white">Sort by</p>
             </div>
           </div>
+
+          {user && (
+            <form
+              onSubmit={addComment}
+              className="flex w-[800px] pt-4 items-start"
+            >
+              <img
+                src={user?.photoURL}
+                alt="profile"
+                className="rounded-full mr-3 h-10 w-10"
+              />
+              <input
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                type="text"
+                placeholder="Add a comment..."
+                className="bg-[transparent] border-b border-b-yt-light-black outline-none text-sm p-1 w-full"
+              />
+            </form>
+          )}
         </div>
       </div>
     </div>
